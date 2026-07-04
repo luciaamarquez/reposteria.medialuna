95d974867976daea7a850dec3e7c4bc65e4876d0
luciaamarquez <luciaamarquez222@gmail.com>
2026-07-04 01:12:07 -0300

    Diseño sección productos

---
+++ productos.php
@@ -0,0 +1,191 @@
+<?php include("includes/header.php"); ?>
+
+<main>
+
+    <section class="banner-productos">
+
+        <h1>Nuestros productos</h1>
+
+        <p>Descubrí nuestra riquísima selección de productos artesanales</p>
+
+    </section>
+
+    <section class="categorias-productos">
+
+        <button>Donuts</button>
+
+        <button class="activo">Budines</button>
+
+        <button>Muffins</button>
+
+        <button>Tortas</button>
+
+        <button>Cookies</button>
+
+    </section>
+
+    <section class="productos-grid">
+
+    <div class="card-producto">
+
+        <a href="producto.php">
+            <img src="img/productos/producto.jpg" alt="Producto">
+        </a>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$7.000</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="card-producto">
+
+        <a href="producto.php">
+            <img src="img/productos/producto.jpg" alt="Producto">
+        </a>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$7.000</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="card-producto">
+
+        <a href="producto.php">
+            <img src="img/productos/producto.jpg" alt="Producto">
+        </a>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$7.000</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+    <!-- Producto agotado -->
+
+    <div class="card-producto agotado">
+
+        <div class="imagen-agotada">
+            No disponible
+        </div>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$ -----</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="card-producto">
+
+        <a href="producto.php">
+            <img src="img/productos/producto.jpg" alt="Producto">
+        </a>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$7.000</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="card-producto">
+
+        <a href="producto.php">
+            <img src="img/productos/producto.jpg" alt="Producto">
+        </a>
+
+        <div class="info-producto">
+
+            <div>
+
+                <h3>Nombre</h3>
+
+                <p>Descripción</p>
+
+                <span>$7.000</span>
+
+            </div>
+
+            <button>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+</section>
+
+</main>
+
+<?php include("includes/footer.php"); ?>
