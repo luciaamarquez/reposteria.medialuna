885cc6e4f5fcaf7a81e7b5d96511fa9a9a1033c7
luciaamarquez <luciaamarquez222@gmail.com>
2026-08-14 16:11:04 -0300

    Corregi codigo

---
+++ recetas.php
@@ -0,0 +1,47 @@
+<?php
+require_once "conexion.php";
+
+$sql = "SELECT * FROM recetas";
+$resultado = $conexion->query($sql);
+?>
+
+<section class="recetas">
+
+    <h2>Nuestras recetas</h2>
+    <p class="subtitulo">
+        Aprendé a preparar deliciosos postres con nuestras recetas.
+    </p>
+
+    <?php while($receta = $resultado->fetch_assoc()) { ?>
+
+    <div class="receta">
+
+        <div class="receta-imagen">
+            <img src="img/<?php echo $receta["imagen"]; ?>" alt="<?php echo $receta["titulo"]; ?>">
+        </div>
+
+        <div class="receta-contenido">
+
+            <h3>🍴 <?php echo $receta["titulo"]; ?></h3>
+
+            <div class="columnas">
+
+                <div class="columna">
+                    <h4>Ingredientes</h4>
+                    <p><?php echo nl2br($receta["ingredientes"]); ?></p>
+                </div>
+
+                <div class="columna">
+                    <h4>Preparación</h4>
+                    <p><?php echo nl2br($receta["preparacion"]); ?></p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php } ?>
+
+</section>
